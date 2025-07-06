cabb99663a7cbc6d71c68692b96025213026f5f6
waltbest2 <waltbest2@github.com>
2025-07-06 18:12:37 +0800

    part

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,11 +1,14 @@
 
 import { initModuleCubeWebcomponent } from './module-cube';
-import { CssSandboxOption, RemoteComponentOption, ServiceOption } from './types';
+import { CssSandboxOption, RemoteComponentOption, ServiceOption, ServiceType } from './types';
 import { genRandomString } from './utils';
 
 declare const globalThis;
+declare const Error;
 
 export const RANDOM_ID_PREFIX = 'Rmc';
+
+const ZoneLabel = 'isServiceZone';
 const ID_PREFIX = 'mc';
 
 /**
@@ -261,6 +264,30 @@ function addParams2Node(node, params) {
   }
 }
 
+/**
+ * 从当前zone向上赵第一个满足条件的zone
+ * @returns 
+ */
+function getServiceIdByCurrentZone(): ServiceType | undefined {
+  if (!globalThis.Zone) {
+    return undefined;
+  }
+
+  let cz = globalThis.Zone.current;
+  while (cz && cz._properties?.[ZoneLabel] !== true) {
+    cz = cz.parent;
+  }
+
+  if (cz) {
+    return {
+      serviceId: cz.name,
+      serviceName: cz._properties.serviceName,
+    }
+  } else {
+    return undefined;
+  }
+}
+
 export class CssSandbox {
 
   /**
@@ -363,6 +390,204 @@ export class CssSandbox {
         this.mcTagName = moduleCubeTagName.toLowerCase();
         this.mcTagNameUpper = moduleCubeTagName.toUpperCase();
       }
+
+      initModuleCubeWebcomponent(this.mcTagName);
+
+      if (Error.stackTraceLimit) {
+        if (maxStack) {
+          Error.stackTraceLimit = maxStack;
+        } else {
+          Error.stackTraceLimit = MAX_STACK_NUM;
+        }
+      }
+
+      this.mainService = '';
+      this.needShadowDom = needShadowDom ?? true;
+      this.needPatched = needPatched ?? true;
+      this.needStackAnlysis = needStackAnlysis ?? false;
+      if (this.needPatched) {
+        this.patch();
+      }
+
+      globalThis.__proto__._CSSSandbox = this;
+      globalThis.__proto__._JSSandbox = {
+        __mcGlobal: {},
+      }
     }
   }
+
+  /**
+   * 根据服务判断是否需要根据stack分析
+   * @param service 
+   * @returns 空字符串表示没有匹配到
+   */
+  private checkServiceStackConfig(service: string): string {
+    const { needStackAnlysis } = this.serviceList[service] || {};
+    if ( needStackAnlysis === false) {
+      return '';
+    }
+
+    return service;
+  }
+
+  /**
+   * 从调用栈获取相关服务
+   * @param callstack 
+   * @param services 
+   * @returns 
+   */
+  private getListFromErrorStack(callstack: string[], services: string[]): string {
+    // 第一次 找到对应的共享服务
+    if (!this.mainService) {
+      this.mainService = getMainService(callstack, services);
+    }
+
+    for (const cskb of callstack) {
+      for (const service of services) {
+        if (service !== this.mainService && cskb.includes(service)) {
+          return this.checkServiceStackConfig(service);
+        }
+      }
+    }
+
+    return this.checkServiceStackConfig(this.mainService);
+  }
+
+  private check(element?: {style?: Node; link?: Node}): ServiceType | undefined {
+    // check 1: 使用style 自带标识判断
+    const services = Object.keys(this.serviceList);
+    for (const service of services) {
+      const { checkService } = this.serviceList[service] || {};
+      if (element && checkService?.(element)) {
+        return {
+          serviceName: service
+        }
+      }
+    }
+
+    // check 2: 借助zone.js判断
+    if (globalThis.Zone) {
+      const service = getServiceIdByCurrentZone();
+      if (service) {
+        return service;
+      }
+    } else {
+      console.warn('[module-cube]: no zone.js, please install zone.js, this time use error stack!')
+    }
+
+    if (!this.needStackAnlysis) {
+      return undefined;
+    }
+
+    // check 3: 兜底， 使用调用栈， 模块联邦共享模式或webpack external或systemjs共享模式下，不能识别出来
+    const callstack = new Error().stack.split('\n');
+    const stackService = this.getListFromErrorStack(
+      callstack.map((url: string) => {
+        return url.substring(url.indexOf('//'));
+      }),
+      services
+    );
+
+    return stackService ? {
+      serviceName: stackService
+    } : undefined;
+  }
+
+  /**
+   * 补充子服务配置信息，支持重复执行，后面设置会覆盖前面设置
+   * @param service 重要：一定是静态url上的服务名
+   * @param option 
+   */
+  public addService(service: string, option?: ServiceOption) {
+    this.serviceList[service] = this.serviceList[service] || {
+      needShadowDom: true,
+      needStackAnlysis: true,
+    };
+
+    Object.assign(this.serviceList[service], option || {});
+
+    this.serviceStyles[service] = this.serviceStyles[service] || new Map<string, Node>();
+
+    this.serviceLinks[service] = this.serviceLinks[service] || new Map<string, Node>();
+
+    const { innerSelectors } = option || {};
+    if (Array.isArray(innerSelectors)) {
+      globalInnerSelector = [...new Set([...globalInnerSelector, ...innerSelectors])];
+    }
+  }
+
+  /**
+   * 获取对应的module-cube
+   * @param service id或服务名，如果是服务名，默认第一个
+   * @param checkGlobal 是否需要在global中查找
+   * @returns 对应某个module-cube
+   */
+  public getHost(service, checkGlobal = false) {
+    const { serviceId, serviceName } = service;
+
+    // 检查是否需要放到body下的全局div中
+    if (checkGlobal) {
+      const { useGlobalDivSandbox } = this.serviceList[serviceName] || {};
+      if (useGlobalDivSandbox) {
+        const globalId = genGlobalDivId(serviceName);
+        if (this.moduleList[globalId]?.host) {
+          return this.moduleList[globalId].host;
+        }
+      }
+    }
+
+    if (this.moduleList[serviceId]) {
+      return this.moduleList[serviceId].host;
+    } else {
+      /**
+       * 出现这个分支场景：
+       * 1. 命中服务名，并不能确定是哪个节点或zone，类似通过style的di或调用栈
+       * 2. webcomponent注册是记录的serviceid和当前serviceid不一致，但前俄面不页面并没有注册时的节点，保持当前页所有相同组件的样式一致
+       */
+      for (const [key, value] of Object.entries(this.moduleList)) {
+        // globalDiv 不能作为组件样式承载容器
+        if (value?.serviceName === serviceName && !key.startsWith(GLOBAL_DIV_PREFIX)) {
+          return value.host;
+        }
+      }
+    }
+
+    return document.head;
+  }
+  
+  private cloneStyle2OtherHost(service, element, includeHead: boolean) {
+    const { serviceId, serviceName } = service;
+
+    // 没有style名内容就不复制，没意义
+    if (!element.textContent) {
+      return;
+    }
+
+    for (const [id, module] of Object.entries(this.moduleList)) {
+      // 如果没有指定serviceId, 并且不是globalDiv,则默认第一个已经被加入style
+      if (!serviceId && module.serviceName === serviceName && !id.startsWith(GLOBAL_DIV_PREFIX)) {
+        continue;
+      }
+
+      const { host } = module;
+      if (module.serviceName === serviceName && serviceId !== id && !getStyleInHost(element.textContent, host)) {
+        const style = cloneStyle(element);
+        this.rawElementAppendChild.call(host, style);
+        patchCSS(host, style.sheet);
+      }
+    }
+
+    if (includeHead) {
+      const style = cloneStyle(element);
+
+      // 记录额外添加到head的style，需要动态清除
+      element._headStyles = element._headStyles || [];
+      element._headStyles.push(style);
+      this.rawElementAppendChild.call(document.ATTRIBUTE_NODE, style);
+    }
+  }
+
+
+
+
 }
